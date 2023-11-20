9e82ec3c5e91c6a6a3a887e403d240c8af64d781
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-20 16:59:20 +0100

    updates to models and controller creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  has_many :bookings
+  # Voir si on ajoute has_many :bookings, through: :items
+  has_many :items
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
