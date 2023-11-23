faa88387dc3438ab6e4af5599fe7b86ad0c9ccbd
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-23 13:18:08 +0100

    booking modal completed

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,10 +2,19 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["start", "date"]
+  static targets = ["start", "end", "totalprice", "dayprice"]
 
   connect() {
     console.log("Test")
     console.log(this.startTarget)
+    console.log(this.endTarget)
+    console.log(this.totalpriceTarget)
+    console.log(this.daypriceTarget)
+  }
+
+  click() {
+    const totalPrice = this.totalpriceTarget;
+    totalPrice.innerHTML = `${((Date.parse(this.endTarget.value) - Date.parse(this.startTarget.value)) / 86400000) * Number(this.daypriceTarget.value)} euros`;
+    totalPrice.style.fontWeight = "bold";
   }
 }
