0b7acdb8fa4a117490bec993babe34c46fab1c9d
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-24 14:28:33 +0100

    upd

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  # get "/items/my_items", to: "items#my_items"
-
   resources :items, only: [:index, :show, :new, :create, :update, :destroy] do
     resources :bookings, only: [:create]
   end
