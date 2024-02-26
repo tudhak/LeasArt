4a0def9b088bdc794d45b8b247c6a02f6e036ccb
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-02-26 16:15:13 +0100

    reworked flash messages for user log in and log out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,14 +9,21 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    flash[:notice] = "Hello, #{resource.first_name} !"
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    flash[:notice] = "You successfully signed out." if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
 
   # protected
 
