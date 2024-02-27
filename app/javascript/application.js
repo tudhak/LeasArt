78c6fa406bca15f27fba50a34b711af32dd9de69
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-02-27 19:23:15 +0100

    WIP - resolving JS execution issues linked to Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@popperjs/core"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+// import { Turbo } from "@hotwired/turbo-rails";
+// Turbo.session.drive = false;
+import "controllers";
+import "@popperjs/core";
+import "bootstrap";
