8a12e5faef3519d126bda0e9e237413f08fbb42c
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-02-27 09:51:20 +0100

    implemented functional datePicker with range

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,9 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
+import rangePlugin from "rangePlugin";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["startDate", "endDate"];
   connect() {
-    flatpickr(this.element);
+    flatpickr(this.startDateTarget, {
+      plugins: [new rangePlugin({ input: this.endDateTarget })],
+    });
+    flatpickr(this.endDateTarget, {});
   }
 }
